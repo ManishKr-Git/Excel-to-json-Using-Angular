f6a932c9206f9fa30db6e9f8f572f3536686be53
ManishKr-Git <manishkumarchoumiyan1998@gmail.com>
2021-02-26 23:50:51 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExcelDemoComponent } from './excel-demo/excel-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExcelDemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
